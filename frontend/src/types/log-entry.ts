0e3058cb931251487ced7bd641c2bec22ab2be08
Divya <86043076+Divya4879@users.noreply.github.com>
2025-03-30 16:54:30 +0530

    Log Entry Interface

---
+++ frontend/src/types/log-entry.ts
@@ -0,0 +1,10 @@
+export interface LogEntry {
+    id: string
+    date: string
+    activity: string
+    duration: number
+    notes: string
+    timeOfDay: string
+    preActivityRating: number
+    postActivityRating: number
+  }
